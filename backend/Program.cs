c93c00a34407840814df0232bd0037326e991697
dplesko2007 <dplesko2007@gmail.com>
2026-02-16 20:02:29 +0300

    Changed Console.cs. 1 commit instead of 3.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет");
-Console.WriteLine("Плеско Данил Дмитриевич");
-Console.WriteLine(DateTime.Now);
+String name = Console.ReadLine();
+Console.WriteLine($"Привет, {name}");
+DateTime now = DateTime.Now;
+Console.WriteLine(now.DayOfWeek);
